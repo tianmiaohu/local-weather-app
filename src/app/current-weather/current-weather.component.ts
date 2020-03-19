13de9890a2c158240250e08c03853fb801ff29d7
tianmiaohu <tianmiaohu@users.noreply.github.com>
2020-03-19 15:05:31 -0400

    Define model using interfaces

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from '../interfaces';
 
 @Component({
   selector: 'app-current-weather',
-  template: `<p>current-weather works!</p>`,
-  //templateUrl: './current-weather.component.html',
+  //template: `<p>current-weather works!</p>`,
+  templateUrl: './current-weather.component.html',
   styles: ['']
   //styleUrls: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent implements OnInit {
+  current: ICurrentWeather;
 
-  constructor() { }
+  constructor() {
+    this.current = {
+      city: 'Bethesda',
+      country: 'US',
+      date: new Date(),
+      image: 'assets/img/sunny.jpg',
+      temperature: 72,
+      description: 'sunny',
+    } as ICurrentWeather;
+  }
 
   ngOnInit(): void {
   }
