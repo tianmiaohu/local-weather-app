7704dc0f78655654d09ce49512f1d6f32ad028ba
tianmiaohu <tianmiaohu@users.noreply.github.com>
2020-03-19 13:22:30 -0400

    Issue #1: display current weather

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-weather',
+  template: `<p>current-weather works!</p>`,
+  //templateUrl: './current-weather.component.html',
+  styles: ['']
+  //styleUrls: ['./current-weather.component.css']
+})
+export class CurrentWeatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
